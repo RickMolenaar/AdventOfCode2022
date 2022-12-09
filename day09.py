b887119dbf37356bb02c9c0bcfd8a8b6c6c4a427
Rick Molenaar <rick@thesustainables.nl>
2022-12-09 07:45:33 +0100

    Day 9 solved

--- day09.py
+++ day09.py
@@ -14,15 +14,16 @@ def move_tail(tail, head):
         return tail
     new_tail = list(tail)
     if tail[0] != head[0]:
-        new_tail[0] -= (tail[0] - head[0]) / abs(tail[0] - head[0])
+        new_tail[0] -= (tail[0] - head[0]) // abs(tail[0] - head[0])
     if tail[1] != head[1]:
-        new_tail[1] -= (tail[1] - head[1]) / abs(tail[1] - head[1])
+        new_tail[1] -= (tail[1] - head[1]) // abs(tail[1] - head[1])
     return tuple(new_tail)
 
 def solve(inp, debug=False):
     inp = format_input(inp)
-    tail = head = (0, 0)
-    visited = {tail}
+    # tail = head = (0, 0)
+    knots = [(0, 0)] * 10
+    visited = {knots[-1]}
     for command in inp:
         direction, amount = command.split()
         if direction == 'R':
@@ -36,11 +37,14 @@ def solve(inp, debug=False):
         else:
             raise ValueError(f'Couldn\'t read {direction}')
         for i in range(int(amount)):
-            head = (head[0] + to_add[0], head[1] + to_add[1])
-            tail = move_tail(tail, head)
-            visited.add(tail)
-        # if debug:
-        #     print(head, tail)
+            # head = (head[0] + to_add[0], head[1] + to_add[1])
+            knots[0] = (knots[0][0] + to_add[0], knots[0][1] + to_add[1])
+            for i in range(1, 10):
+                # print(i, len(knots))
+                knots[i] = move_tail(knots[i], knots[i-1])
+            visited.add(knots[-1])
+        if debug:
+            print(knots)
     return len(visited)
 
 def main(debug = False):
