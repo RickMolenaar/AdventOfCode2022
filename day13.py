3ef100cf18ee27411765b2f6c392759af130f2a6
Rick Molenaar <rick@thesustainables.nl>
2022-12-13 08:04:15 +0100

    Day 13 solved

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+import functools
+
 def parse_input(file = 'day13.txt'):
     with open(file) as f:
         s = map(lambda l: l.rstrip(), f.readlines())
@@ -7,10 +9,10 @@ def parse_example():
     return parse_input('day13example.txt')
 
 def format_input(inp):
-    pairs = []
+    packets = []
     for i in range(0, len(inp), 3):
-        pairs.append((eval(inp[i]), eval(inp[i+1])))
-    return pairs
+        packets.extend((eval(inp[i]), eval(inp[i+1])))
+    return packets
 
 def compare(left, right, debug, level = 0):
     if debug:
@@ -37,12 +39,23 @@ def compare(left, right, debug, level = 0):
     else:
         return c0
 
+def key_compare(packet1, packet2):
+    return -1 if compare(packet1, packet2, False) else 1
+
 def solve(inp, debug=False):
-    pairs = format_input(inp)
-    ordered_pairs = [i+1 for i, pair in enumerate(pairs) if compare(pair[0], pair[1], False) is True]
+    # pairs = format_input(inp)
+    # ordered_pairs = [i+1 for i, pair in enumerate(pairs) if compare(pair[0], pair[1], False) is True]
+    # if debug:
+    #     print(ordered_pairs)
+    # return sum(ordered_pairs)
+    packets = format_input(inp)
+    packets.append([[2]])
+    packets.append([[6]])
+    packets = sorted(packets, key=functools.cmp_to_key(key_compare))
     if debug:
-        print(ordered_pairs)
-    return sum(ordered_pairs)
+        for p in packets:
+            print(p)
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
 
 def main(debug = False):
     return str(solve(parse_example(), True)) + '\n' + str(solve(parse_input(), debug))
