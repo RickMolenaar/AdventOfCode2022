77b76703adbb8ca11530901ba8dab76682e08839
Rick Molenaar <rick_molenaar96@live.nl>
2022-12-18 12:56:03 +0100

    Day 18 solved

---
+++ day18.py
@@ -0,0 +1,64 @@
+def parse_input(file = 'day18.txt'):
+    with open(file) as f:
+        s = map(lambda l: l.rstrip(), f.readlines())
+    return list(s)
+
+def parse_example():
+    return parse_input('day18example.txt')
+
+def format_input(inp):
+    rocks = set()
+    min_x = min_y = min_z = max_x = max_y = max_z = 0
+    for line in inp:
+        rock = tuple(map(int, line.split(',')))
+        rocks.add(rock)
+        min_x = min(min_x, rock[0])
+        min_y = min(min_y, rock[1])
+        min_z = min(min_z, rock[2])
+        max_x = max(max_x, rock[0])
+        max_y = max(max_y, rock[1])
+        max_z = max(max_z, rock[2])
+    return rocks, min_x - 1, min_y - 1, min_z - 1, max_x + 1, max_y + 1, max_z + 1
+
+def solve(inp, debug=False):
+    rocks, min_x, min_y, min_z, max_x, max_y, max_z = format_input(inp)
+    # total_sides = 0
+    # for rock in rocks:
+    #     for d in (-1, 1):
+    #         for dir in range(3):
+    #             to_check = list(rock)
+    #             to_check[dir] += d
+    #             if tuple(to_check) not in rocks:
+    #                 total_sides += 1
+    blocks_to_check = set()
+    blocks_to_check.add((min_x, min_y, min_z))
+    checked = set()
+    semi_open_blocks = set()
+    while blocks_to_check:
+        # print(blocks_to_check)
+        (x, y, z) = blocks_to_check.pop()
+        for d in (-1, 1):
+            for dir in range(3):
+                to_check = [x, y, z]
+                to_check[dir] += d
+                to_check = tuple(to_check)
+                if to_check in rocks:
+                    semi_open_blocks.add((x, y, z))
+                    continue
+                if to_check in checked:
+                    continue
+                if min_x <= to_check[0] <= max_x and min_y <= to_check[1] <= max_y and min_z <= to_check[2] <= max_z:
+                    blocks_to_check.add(to_check)
+        checked.add((x, y, z))
+    total_sides = 0
+    for block in semi_open_blocks:
+        for d in (-1, 1):
+            for dir in range(3):
+                to_check = list(block)
+                to_check[dir] += d
+                if tuple(to_check) in rocks:
+                    total_sides += 1
+    return total_sides
+
+def main(debug = False):
+    return str(solve(parse_example(), True)) + '\n' + str(solve(parse_input(), debug))
